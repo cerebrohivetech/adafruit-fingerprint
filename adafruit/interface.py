4cf6f3626af7dd993ba8a0e6e948b6984995f45a
Faith Odonghanro <toriboi.fo@gmail.com>
2019-11-05 10:59:55 +0100

    Implement down_char, store and search methods

--- adafruit/interface.py
+++ adafruit/interface.py
@@ -3,6 +3,7 @@
 from adafruit.core import Package
 from adafruit.exceptions import *
 from adafruit.response import *
+from adafruit.utils import hexbyte_2integer_normalizer
 
 
 class AdafruitFingerprint:
@@ -90,20 +91,84 @@ class AdafruitFingerprint:
                 raise UnknownConfirmationCodeException('Unknown confirmation code')
         raise SerialReadException('No data read from serial port')
 
+
+    # to upload the character file or template of CharBuffer1/CharBuffer2 to upper computer
     def up_char(self, buffer):
         data = [0x08, buffer]
-        self.package.write(data=data, show=False)
+        self.package.write(data=data, show=True)
 
-        serial_data = self.package.read(show=False)
+        serial_data = self.package.read(show=True)
         if len(serial_data):
             package_content = serial_data[4]
             if package_content == FINGERPRINT_OK:
-                template = self.package.read_template()
+                template = self.package.read_template(show=True)
                 return FINGERPRINT_OK, template
             elif package_content == FINGERPRINT_PACKETRECEIVER:
                 return FINGERPRINT_PACKETRECEIVER
             elif package_content == FINGERPRINT_TEMPLATEUPLOADFAIL:
                 return FINGERPRINT_TEMPLATEUPLOADFAIL
+            else:
+                raise UnknownConfirmationCodeException('Unknown confirmation code')
+        raise SerialReadException('No data read from serial port')
+
+    # to download character file or template from upper computer to the specified
+    # buffer of Module
+    def down_char(self, buffer, template):
+        data = [0x09, buffer]
+        self.package.write(data=data, show=True)
+
+        serial_data = self.package.read(show=True)
+        if len(serial_data):
+            package_content = serial_data[4]
+            if package_content == FINGERPRINT_OK:
+                self.package.write_template(data=template, show=True)
+                return FINGERPRINT_OK
+            elif package_content == FINGERPRINT_PACKETRECEIVER:
+                return FINGERPRINT_PACKETRECEIVER
+            elif package_content == FINGERPRINT_TEMPLATEDOWNLOADFAIL:
+                return FINGERPRINT_TEMPLATEDOWNLOADFAIL
+            else:
+                raise UnknownConfirmationCodeException('Unknown confirmation code')
+        raise SerialReadException('No data read from serial port')
+
+    # to store the template of specified buffer (Buffer1/Buffer2) at the designated
+    # location of Flash library
+    def store(self, buffer, page):
+        data = [0x06, buffer, 0x00, page]
+        self.package.write(data=data, show=True)
+
+        serial_data = self.package.read(show=True)
+        if len(serial_data):
+            package_content = serial_data[4]
+            if package_content == FINGERPRINT_OK:
+                return FINGERPRINT_OK
+            elif package_content == FINGERPRINT_PACKETRECEIVER:
+                return FINGERPRINT_PACKETRECEIVER
+            elif package_content == FINGERPRINT_BADLOCATION:
+                return FINGERPRINT_BADLOCATION
+            elif package_content == FINGERPRINT_FLASHER:
+                return FINGERPRINT_FLASHER
+            else:
+                raise UnknownConfirmationCodeException('Unknown confirmation code')
+        raise SerialReadException('No data read from serial port')
+
+    # to search the whole finger library for the template that matches the one in
+    # CharBuffer1 or CharBuffer2. When found, PageID will be returned
+    def search(self, buffer, page_start, page_num):
+        data = [0x04, buffer, 0x00, page_start, 0x00, page_num]
+        self.package.write(data=data, show=True)
+
+        serial_data = self.package.read(show=True)
+        if len(serial_data):
+            package_content = serial_data[4]
+            if package_content == FINGERPRINT_OK:
+                page = hexbyte_2integer_normalizer(serial_data[5], serial_data[6])
+                confidence_score = hexbyte_2integer_normalizer(serial_data[7], serial_data[8])
+                return FINGERPRINT_OK, page, confidence_score
+            elif package_content == FINGERPRINT_PACKETRECEIVER:
+                return FINGERPRINT_PACKETRECEIVER
+            elif package_content == FINGERPRINT_NOTFOUND:
+                return FINGERPRINT_NOTFOUND
             else:
                 raise UnknownConfirmationCodeException('Unknown confirmation code')
         raise SerialReadException('No data read from serial port')
