0564c26ad7a6b905e80d2bd59465ea69ddd8c203
Faith Odonghanro <toriboi.fo@gmail.com>
2019-12-25 16:33:25 +0100

    Include module docstrings
    Rename function parameters
    
    Rename function local variables

--- adafruit_fingerprint/utils.py
+++ adafruit_fingerprint/utils.py
@@ -1,8 +1,37 @@
-def hexbyte_2integer_normalizer(first_2byte, second_2btye):
-    first = f'{hex(first_2byte)}'.lstrip('0x')
-    second = f'{hex(second_2btye)}'.lstrip('0x')
-    first = first if len(f'{first}') == 2 else f'0{first}'
-    second = second if len(f'{second}') == 2 else f'0{second}'
-    hex_string = f'{first}{second}'
+"""Uitility functions used by core interface
+
+This module contains functions that are unnecessary to go in main core
+classes and perform specific functions
+
+Functions
+_________
+hexbyte_2integer_normalizer(first_int_byte, second_int_btye)
+    Function to normalize integer bytes to a single byte
+
+"""
+
+
+def hexbyte_2integer_normalizer(first_int_byte, second_int_btye):
+    """Function to normalize integer bytes to a single byte
+
+    Transform two integer bytes to their hex byte values and normalize
+    their values to a single integer
+
+    Parameters
+    __________
+    first_int_byte, second_int_byte : int
+        integer values to normalize (0 to 255)
+
+    Returns
+    _______
+    integer: int
+        Single normalized integer
+    """
+
+    first_hex = f'{hex(first_int_byte)}'.lstrip('0x')
+    second_hex = f'{hex(second_int_btye)}'.lstrip('0x')
+    first_hex = first_hex if len(f'{first_hex}') == 2 else f'0{first_hex}'
+    second_hex = second_hex if len(f'{second_hex}') == 2 else f'0{second_hex}'
+    hex_string = f'{first_hex}{second_hex}'
     integer = int(hex_string, 16)
     return integer
