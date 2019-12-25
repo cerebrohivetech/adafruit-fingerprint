f8ecc578d4e279706e96ab2cad37a9695a410fd7
Faith Odonghanro <toriboi.fo@gmail.com>
2019-12-25 14:30:32 +0100

    Include module docstrings

--- adafruit_fingerprint/exceptions.py
+++ adafruit_fingerprint/exceptions.py
@@ -1,10 +1,26 @@
+"""Internal Exception classes used by package
+
+These classes subclass the base Exception class
+
+Classes
+_______
+MissingPortException(Exception)
+SerialReadException(Exception)
+UnknownConfirmationCodeException(Exception)
+
+"""
+
+
 class MissingPortException(Exception):
-    pass
+    """
+    Exception raised when the port param is missing when instantiating
+    the AdafruitFingerprin class
+    """
 
 
 class SerialReadException(Exception):
-    pass
+    """Exception raised when no data is read from the serial port"""
 
 
 class UnknownConfirmationCodeException(Exception):
-    pass
+    """Exception raised when package content is an invalid response"""
